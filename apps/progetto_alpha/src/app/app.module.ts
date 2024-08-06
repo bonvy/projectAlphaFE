837247e5682a3f3e425401e3d55a80e2f56eb8fc
bonvy <francesco.bonvecchio01@gmail.com>
2024-08-06 18:23:55 +0200

    refactor

--- apps/progetto_alpha/src/app/app.module.ts
+++ apps/progetto_alpha/src/app/app.module.ts
@@ -61,6 +61,12 @@ const imports =[
     ChartModule,
     RouterOutlet,
     InputTextModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
 
   ],
   providers: [
